5f581279a614836b643c396b64f2db80de70d353
Paul Fletes <paul.fletes@gmail.com>
2022-11-22 13:26:02 -0600

    update /results route in app.py s d

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from pprint import PrettyPrinter
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, send_file
-from geopy.geocoders import Nominatim
+#from geopy.geocoders import Nominatim
 
 
 ################################################################################
-## SETUP
+# SETUP
 ################################################################################
 
 app = Flask(__name__)
@@ -19,12 +19,9 @@ load_dotenv()
 
 pp = PrettyPrinter(indent=4)
 
-API_KEY = os.getenv('API_KEY')
-API_URL = 'http://api.openweathermap.org/data/2.5/weather'
-
 
 ################################################################################
-## ROUTES
+# ROUTES
 ################################################################################
 
 @app.route('/')
@@ -36,45 +33,42 @@ def home():
     }
     return render_template('home.html', **context)
 
+
 def get_letter_for_units(units):
     """Returns a shorthand letter for the given units."""
     return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+
 
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
     # TODO: Use 'request.args' to retrieve the city & units from the query
     # parameters.
-    city = ''
-    units = ''
+    city = request.args.get('city')
+    units = request.args.get('units')
+    api_key = os.getenv('API_KEY')
+    API_URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units={units}&appid={api_key}'
 
-    params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
-        # the city, and the units (metric or imperial).
-        # See the documentation here: https://openweathermap.org/current
-
-    }
-
-    result_json = requests.get(API_URL, params=params).json()
+    result_json = requests.get(API_URL).json()
 
     # Uncomment the line below to see the results of the API call!
-    # pp.pprint(result_json)
+    pp.pprint(result_json)
 
     # TODO: Replace the empty variables below with their appropriate values.
     # You'll need to retrieve these from the result_json object above.
 
     # For the sunrise & sunset variables, I would recommend to turn them into
-    # datetime objects. You can do so using the `datetime.fromtimestamp()` 
+    # datetime objects. You can do so using the `datetime.fromtimestamp()`
     # function.
     context = {
         'date': datetime.now(),
-        'city': '',
-        'description': '',
-        'temp': '',
-        'humidity': '',
-        'wind_speed': '',
-        'sunrise': '',
-        'sunset': '',
+        'city': result_json['name'],
+        'description': result_json['weather'][0]['description'],
+        'temp': result_json['main']['temp'],
+        'humidity': result_json['main']['humidity'],
+        'wind_speed': result_json['wind']['speed'],
+        'sunrise': datetime.fromtimestamp(result_json['sys']['sunrise']),
+        'sunset': datetime.fromtimestamp(result_json['sys']['sunset']),
         'units_letter': get_letter_for_units(units)
     }
 
@@ -90,13 +84,12 @@ def comparison_results():
     city2 = ''
     units = ''
 
-    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
+    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a
     # helper function for this!
-
 
     # TODO: Pass the information for both cities in the context. Make sure to
     # pass info for the temperature, humidity, wind speed, and sunset time!
-    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
+    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and
     # `city2_info`, to organize the data.
     context = {
 
