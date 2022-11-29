4d4720dcd9ee327e70aadc9cea7de6fb5ca83055
Paul Fletes <paul.fletes@gmail.com>
2022-11-29 16:52:03 -0600

    clear unnecessary comments and complete assignment

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
-# Get the API key from the '.env' file
+
 load_dotenv()
 
 pp = PrettyPrinter(indent=4)
@@ -42,8 +42,6 @@ def get_letter_for_units(units):
 @app.route('/results')
 def results():
     """Displays results for current weather conditions."""
-    # TODO: Use 'request.args' to retrieve the city & units from the query
-    # parameters.
     city = request.args.get('city')
     units = request.args.get('units')
     api_key = os.getenv('API_KEY')
@@ -51,15 +49,8 @@ def results():
 
     result_json = requests.get(API_URL).json()
 
-    # Uncomment the line below to see the results of the API call!
     pp.pprint(result_json)
 
-    # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the result_json object above.
-
-    # For the sunrise & sunset variables, I would recommend to turn them into
-    # datetime objects. You can do so using the `datetime.fromtimestamp()`
-    # function.
     datetime_object = (datetime.now()).strftime('%A, %B %d, %Y')
 
     context = {
@@ -80,8 +71,6 @@ def results():
 @app.route('/comparison_results')
 def comparison_results():
     """Displays the relative weather for 2 different cities."""
-    # TODO: Use 'request.args' to retrieve the cities & units from the query
-    # parameters.
     city1 = request.args.get('city1')
     city2 = request.args.get('city2')
     units = request.args.get('units')
@@ -89,13 +78,6 @@ def comparison_results():
     city2_data = compare_city_info(city2, units)
     datetime_object = (datetime.now()).strftime('%A, %B, %d, %Y')
 
-    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a
-    # helper function for this!
-
-    # TODO: Pass the information for both cities in the context. Make sure to
-    # pass info for the temperature, humidity, wind speed, and sunset time!
-    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and
-    # `city2_info`, to organize the data.
     context = {
         'city1': city1_data,
         'city2': city2_data,
@@ -109,6 +91,7 @@ def comparison_results():
 
 
 def compare_city_info(city, units):
+    '''Helper function comparing 2 cities'''
     api_key = os.getenv('API_KEY')
     response = requests.get(
         f'https://api.openweathermap.org/data/2.5/weather?q={city}&units={units}&appid={api_key}')
